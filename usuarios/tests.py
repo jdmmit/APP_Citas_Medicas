4823ddc2749c78a3f0b573e7d52bdae09a84df51
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-07-31 21:19:50 -0500

    ci: corregir configuración de GitHub Actions y agregar prueba básica

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """Prueba básica para verificar que 1 + 1 = 2"""
+        self.assertEqual(1 + 1, 2)
